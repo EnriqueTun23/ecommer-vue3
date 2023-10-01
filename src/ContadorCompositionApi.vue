2db147f819333c6ea05761d35f38c774b64d0813
EnriqueTunXix <135084799+EnriqueTunXix@users.noreply.github.com>
2023-09-30 23:16:13 -0600

    feat: The composition api and option api demos were added

---
+++ src/ContadorCompositionApi.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Soy el composition</h1>
+    <p>Contador: {{ count }}</p>
+    <button @click="increment">Incrementar 2</button>
+</template>
+
+
+<script setup lang="ts">
+    import { onMounted, ref } from 'vue'
+    const count = ref(0)
+
+
+    const increment = () => {
+        count.value += 1
+    }
+
+    onMounted(() => {
+        console.log('El valor inicial del contador es' + count.value)
+    })
+</script>
---
+++ src/ContadorOptionsApi.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>Contador: {{ count }}</h1>
+    <button @click="increment">Aumentar contador</button>
+</template>
+<script lang="ts">
+ export default {
+    data() {
+        return {
+            count: 0
+        }
+    },
+    methods: {
+        increment() {
+            this.count *= 1
+        }
+    },
+    mounted() {
+        console.log('el +valor inicial es', this.count)
+    },
+    
+ }
+</script>
